6aede5c59efe9d4e9c1326e3d294c5eccd0ea9b1
Yan Arlex Vallejo <yavallejor@gmail.com>
2018-11-07 21:07:52 -0500

    12 - agregamos el cambio final para el index.php

--- index.php
+++ index.php
@@ -1,222 +1,91 @@
-<?php get_header(); ?>
-    <main class="main">
-      <section class="scrollto photostyles box box--bottom-padding" id="photostyles">
-        <h2 class="text text-center title title--main">Photo Styles</h2>
-        <span class="text text-center title title--secundary">We strive to have a wide range of styles in photography. We love to mix and make photography an unforgettable memory.</span>
-        <article class="photostyles__item text text-center">
-          <h3 class="title title--section-secundary text text-center">Services</h3>
-            <div class="line line--min"></div>
-            <section class="photostyles__item text margin--big text-center">
-                <article class="photostyles__box js-photostyles--left">
-                  <span class="isa-graduation"></span>
-                  <h3 class="photostyles--title">Aerials</h3>
-                  <p class="photostyles--information">
-                    Aerial photography is the taking of photographs of the ground from an elevated/direct-down position
-                  </p>
-                  <a class="btn btn--primary" href="services-aerials.html">
-                    Read More
-                  </a>
-                </article> <!-- Services one -->
-                <article class="photostyles__box js-photostyles--left">
-                  <span class="isa-photo-camera"></span>
-                  <h3 class="photostyles--title">Europe</h3>
-                  <p class="photostyles--information">
-                    Aerial photography is the taking of photographs of the ground from an elevated/direct-down position
-                  </p>
-                  <a class="btn btn--primary" href="services-aerials.html">
-                    Read More
-                  </a>
-                </article> <!-- Services two -->
-                <article class="photostyles__box js-photostyles--right">
-                  <span class="isa-wedding"></span>
-                  <h3 class="photostyles--title">Pacific</h3>
-                  <p class="photostyles--information">
-                    Aerial photography is the taking of photographs of the ground from an elevated/direct-down position
-                  </p>
-                  <a class="btn btn--primary" href="services-aerials.html">
-                    Read More
-                  </a>
-                </article> <!-- Services two -->
-            </section> <!-- End Services -->
-        </article>
-      </section> <!-- End photostyles -->
-      <section class="scrollto text text-center project" id="project">
-        <h2 class="text text-center title title--main">My Last Work</h2>
-        <!-- Here we place the controls-->
-        <!-- In this section we use the library filterizr.js, for more information visit http://yiotis.net/filterizr/ -->
+<?php
+  get_header();
+?>  
+    <main class="main main--internal">
+      <section class="blog blog__boxing">
         <div class="box">
-          <ul class="list-none controls__work">
-            <li data-filter="all">All Items</li>
-            <li data-filter="1">Aerials</li>
-            <li data-filter="2">Europe</li>
-            <li data-filter="3">Pacific</li>
-            <li data-filter="4">Asia</li>
-            <li data-filter="5">Best Of</li>
-            <li data-filter="6">Africa</li>
-            <li data-filter="7">South America</li>
-          </ul> <!-- End controls -->
-          <div class="filtr-container js-animation-bounceInUp">
-            <div class="filtr-item" data-category="1, 7" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-1.jpg" alt="Aerials">
-                <figcaption>
-                  <h3>Lunar <span>Moon</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item one -->
-            <div class="filtr-item" data-category="2, 1, 5" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-2.jpg" alt="Africa">
-                <figcaption>
-                  <h3>Happy <span>Couple</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item two -->
-            <div class="filtr-item" data-category="3, 6, 1" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-3.jpg" alt="Asia">
-                <figcaption>
-                  <h3>Best <span>Drinks</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item three -->
-            <div class="filtr-item" data-category="4, 2, 5" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-4.jpg" alt="Best Of">
-                <figcaption>
-                  <h3>Best <span>Juice</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item four -->
-            <div class="filtr-item" data-category="5, 1, 3" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-5.jpg" alt="Europe">
-                <figcaption>
-                  <h3>London <span>Street</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item five -->
-            <div class="filtr-item" data-category="6, 3, 5" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-6.jpg" alt="Pacific">
-                <figcaption>
-                  <h3>Boy <span>Girl</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item six -->
-            <div class="filtr-item" data-category="1, 4, 2" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-7.jpg" alt="Aerials">
-                <figcaption>
-                  <h3>Girl<span>Beatiful</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item seven -->
-            <div class="filtr-item" data-category="2, 3, 6" data-sort="value">
-              <figure class="animation__hover"><img src="<?php echo get_template_directory_uri(); ?>/img/my-last-work-8.jpg" alt="Aerials">
-                <figcaption>
-                  <h3>Man <span>Polo</span></h3>
-                </figcaption><a href="#"></a>
-              </figure>
-            </div> <!-- Item eight -->
-          </div>
-        </div>
-      </section><!-- End project -->
-      <section class="isabella scrollto" id="isabella">
-        <article class="isabella__image"></article> <!-- Image Background CSS -->
-        <article class="isabella__information">
-          <span class="text--uppercase">50 high quality images</span>
-          <h4 class="text--uppercase"> Lifestyle & Travel</h4>
-          <hr>
-          <em>My name is  Isabella, the owner & photographer of Isabella photography. I am based in San Francisco, United States</em>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy text of the printing and typesetting. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          <p> Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry. Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
-          <a class="btn btn--slider btn--slider--white" href="services-aerials.html" title="View Gallery">
-            View Gallery
-          </a>
-        </article> <!-- Information right Isabella -->
-      </section> <!-- End isabella -->
-      <section class="testimonials text text-center scrollto" id="testimonials">
-        <span class="title title--section-secundary text text-center text--uppercase">
-          our customers
-        </span>
-        <div class="testimonials__slides">
-          <div>
+          <?php
+            /* Start the Loop */
+            while ( have_posts() ) : the_post();
+          ?>
+
+          <article class="blog__information">
             <figure>
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/testimonial-1.jpg" alt="Testimonial 1" width="114" height="114">
+              <?php
+                if ( has_post_thumbnail() ) { 
+                    the_post_thumbnail('blog', ['class' => 'img-responsive'] );
+                }else{
+              ?>
+                    <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/single-blog.jpg" alt="Isabella" width="800" height="350">
+              <?php
+                }
+              ?>
+              
             </figure>
-            <blockquote>" Sometimes when I'm talking to other people, my words can't keep up with my thoughts. I wonder why we think faster than we speak."</blockquote>
-            <strong> Pelican Steve,</strong>
-            <span> Marketing Manager</span>
-            <span class="text--basecolor">Hubboard Media</span>
-          </div> <!-- End Terstimonial one -->
-          <div>
-            <figure>
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/testimonial-2.jpg" alt="Testimonial 2" width="114" height="114">
-            </figure>
-            <blockquote>" Thank you. before I begin, I'd like everyone to notice that my report is in a professional, clear plastic binder...When a report looks this good."</blockquote>
-            <strong> Max Conversion,</strong>
-            <span> CFO</span>
-            <span class="text--basecolor"> Creative Space Inc.</span>
-          </div> <!-- End Terstimonial two -->
-          <div>
-            <figure>
-              <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/testimonial-3.jpg" alt="Testimonial 3" width="114" height="114">
-            </figure>
-            <blockquote>" My behaviour is addictive functioning in a disease process of toxic co-dependency. I need holistic healing and wellness before. "</blockquote>
-            <strong> eleanor faint,</strong>
-            <span> Developer</span>
-            <span class="text--basecolor"> WikiMedia</span>
-          </div> <!-- End Terstimonial three -->
+            <h2 class="title title--secundary text-center">Camera / Web Design</h2>
+            <h1 class="title title--section text-center"><?php the_title(); ?></h1>
+            
+            <?php
+              the_content();
+            ?>
+
+          </article> <!-- End Blog information-->
+
+          <?php
+            endwhile; // End of the loop.
+        ?>
+
+          <aside class="sidebar">
+            <div class="widget">
+              <h5 class="title title--section text-center">News Recients</h5>
+              <ul class="list-none">
+                <li><a href="#">Steve's DigiCams</a></li>
+                <li><a href="#">Digital Photo Review</a></li>
+                <li><a href="#">Photography Bay</a></li>
+                <li><a href="#">Black Nikon</a></li>
+                <li><a href="#">EOS 7D Mark II</a></li>
+                <li><a href="#">YI 4K Action</a></li>
+                <li><a href="#">Sigma 100-400mm</a></li>
+                <li><a href="#">Canon PowerShot</a></li>
+                <li><a href="#">Palette Gear Review</a></li>
+                <li><a href="#">Fujifilm FinePix XP120</a></li>
+              </ul>
+            </div> <!-- End Widget one -->
+            <div class="widget">
+              <h5 class="title title--section text-center">Follow Us Now</h5>
+              <ul class="list-none rs">
+                <li><a href="https://www.facebook.com/adathemeoficial" title="Facebook AdaTheme" target="_blank"><span class="isa-facebook2"></span></a></li>
+                <li><a href="https://www.twitter.com/adathemeoficial" title="Twitter AdaTheme" target="_blank"><span class="isa-twitter"></span></a></li>
+                <li><a href="https://www.google.com/adathemeoficial" title="Google Plus AdaTheme" target="_blank"><span class="isa-google-plus2"></span></a></li>
+                <li><a href="https://www.instagram.com/adathemeoficial" title="Instagram AdaTheme" target="_blank"><span class="isa-instagram"></span></a></li>
+                <li><a href="https://www.youtube.com/adathemeoficial" title="Youtube AdaTheme" target="_blank"><span class="isa-youtube2"></span></a></li>
+              </ul>
+            </div> <!-- End Widget one -->
+            <div class="widget widget--dashed">
+              <h5 class="title title--section text-center">Categories</h5>
+              <ul class="list-none">
+                <li><a href="#">Abstract</a><a href="#">13</a></li>
+                <li><a href="#">Time Lapse</a><a href="#">14</a></li>
+                <li><a href="#">Still Life</a><a href="#">15</a></li>
+                <li><a href="#">Wedding</a><a href="#">20</a></li>
+                <li><a href="#">Fashion</a><a href="#">28</a></li>
+                <li><a href="#">Advertising</a><a href="#">36</a></li>
+                <li><a href="#">Portrait</a><a href="#">50</a></li>
+                <li><a href="#">Wildlife</a><a href="#">51</a></li>
+                <li><a href="#">Travel</a><a href="#">69</a></li>
+              </ul>
+            </div> <!-- End Widget one -->
+            <div class="widget">
+              <figure>
+                <a href="#" title="Canon Camera">
+                  <img src="img/blog-5.jpg" alt="Canon Camera" width="800" height="1200">
+                </a>
+              </figure>
+            </div> <!-- End Widget one -->
+          </aside> <!-- End Sidebar-->
         </div>
-      </section> <!-- End testimonials -->
-      <section class="blog scrollto" id="blog">
-        <div class="box">
-          <span class="title title--section-secundary text text-center text--uppercase">
-            latest news
-          </span>
-          <div class="blog__item">
-            <figure class="blog__item--new">
-              <div class="image">
-                <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/blog-1.jpg" alt="Steve's DigiCams" width="800" height="1020">
-              </div>
-              <figcaption>
-                <div class="date">
-                  <span class="day">02</span>
-                  <span class="month">Oct</span>
-                </div>
-                <h3>Steve's DigiCams</h3>
-                <p>The Steve's DigiCams site includes a blog on the front page, which monitors all of the latest digital.</p>
-              </figcaption>
-              <a href="steve-digicams.html" title="Steve's DigiCams"></a>
-            </figure> <!-- Item Blog one -->
-            <figure class="blog__item--new">
-              <div class="image">
-                <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/blog-2.jpg" alt="Digital Photo Review" width="800" height="1020">
-              </div>
-              <figcaption>
-                <div class="date">
-                  <span class="day">07</span>
-                  <span class="month">Nov</span>
-                </div>
-                <h3>Digital Photo Review</h3>
-                <p>This site is heavy on the latest digital camera news, which is one of its best features. The blog also keeps.</p>
-              </figcaption>
-              <a href="steve-digicams.html" title="Digital Photo Review"></a>
-            </figure> <!-- Item Blog two -->
-            <figure class="blog__item--new">
-              <div class="image">
-                <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/blog-3.jpg" alt="Photography Bay" width="800" height="1020">
-              </div>
-              <figcaption>
-                <div class="date">
-                  <span class="day">15</span>
-                  <span class="month">Dec</span>
-                </div>
-                <h3>Photography Bay</h3>
-                <p>If you're interested in learning more about the latest DSLR camera accessory gear, the Photography Bay.</p>
-              </figcaption>
-              <a href="steve-digicams.html" title="Photography Bay"></a>
-            </figure> <!-- Item Blog Three -->
-          </div>
-          <!-- Button all Blogs -->
-          <a class="btn btn--center btn--center--big" href="archive.html" title="View all blog posts">
-            View all blog posts
-          </a>
-        </div>
-      </section> <!-- End blog -->
+      </section>
     </main> <!-- End Main -->
-<?php get_footer(); ?>
+<?php
+  get_footer();
+?>
